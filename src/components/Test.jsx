baf91b0f23514bddae148bf6a0cac43a9ca33961
Larson Collier <collierlarson@gmail.com>
2023-05-11 15:02:49 -0400

    added user form

--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -3,7 +3,6 @@ import { db } from '../firebase';
 import { doc, setDoc } from "firebase/firestore";
 
 // Add a new document in collection "cities"
-
 await setDoc(doc(db, "cities", "LA"), {
     name: "Los Angeles",
     state: "CA",
@@ -11,33 +10,40 @@ await setDoc(doc(db, "cities", "LA"), {
 });
 
 export const Test = () => {
+    const [name, setName]= useState('')
+    const [email, setEmail]= useState('')
+    const [username, setUsername]= useState('')
 
-    const handleClick = async () => {
+    const postCities = async () => {
         await setDoc(doc(db, "cities", "LA"), {
             name: "Los Angeles",
             state: "CA",
             country: "USA"
         });
     }
-    
-    // const [data, setData] = useState([]);
+  
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        let userObject = {
+            "name": name,
+            "username": name,
+            "email": email
+        }
 
-    // useEffect(() => {
-    //     db.collection('testCollection')
-    //         .get()
-    //         .then(querySnapshot => {
-    //             const docs = [];
-    //             querySnapshot.forEach(doc => {
-    //                 docs.push(doc.data());
-    //             });
-    //             setData(docs);
-    //         });
-    // }, []);
+        console.log(userObject)
+    }
 
     return (
-        <div>
-            <button onClick={handleClick}>POST LA to CITIES</button>
-        </div>
+        <>
+            <button onClick={postCities}>POST LA to CITIES</button>
+
+            <form onSubmit={handleSubmit}>
+                <input type="text" placeholder='Name...' value={name} onChange={e=>setName(e.target.value)}></input>
+                <input type="text" placeholder='Username...' value={username} onChange={e=>setUsername(e.target.value)}></input>
+                <input type="text" placeholder='E-Mail...' value={email} onChange={e =>setEmail(e.target.value)} ></input>
+                <button>Submit</button>
+            </form>
+        </>
     );
 }
 
